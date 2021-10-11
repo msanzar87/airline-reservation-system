8aa15f14fae0aeb4b279e545d57a56e7a50156c4
munir sanzar <msanzar87@gmail.com>
2021-10-11 00:50:25 -0400

    first commit

---
+++ airline-reservation/src/Main1.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Main1 {
+    public static void main(String[] args) {
+    Person person = new Person("Munir","USA","1/11/1111",5);
+        System.out.println(person.getDateOfBirth());
+    }
+}
