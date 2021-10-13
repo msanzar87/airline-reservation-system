fad53af2074551f568b34bea43d23f5541d91e84
munir sanzar <msanzar87@gmail.com>
2021-10-13 03:54:32 -0400

    Vending Machine1 completed

---
+++ vending-machine/src/MainVM.java
@@ -0,0 +1,11 @@
+public class MainVM {
+    public static void main(String[] args) {
+//Item[][] items = new Item[3][9];
+Item pepsi = new Item("Pepsi",2.99,1);
+                System.out.println("\t************************************************");
+                System.out.println("\t             WELCOME TO JAVA DRINKS!            ");
+                System.out.println("\t************************************************");
+        System.out.println(pepsi);
+
+    }
+}
