f60c40eb0143074211c5abf5eda7e624610ab269
munir sanzar <msanzar87@gmail.com>
2021-10-13 03:23:57 -0400

    Car10 completed

--- CarDealer/src/MainCar.java
+++ CarDealer/src/MainCar.java
@@ -1,16 +1,23 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class MainCar {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         Car[] cars = new Car[]{
                 new Car("Nissan", 5000, 2020, "red", new String[]{"tires", "keys"}),
                 new Car("Dodge", 11000, 2019, "blue", new String[]{"tires", "keys"}),
                 new Car("Nissan", 5000, 2017, "yellow", new String[]{"tires", "filter"}),
         };
         Dealership dealership = new Dealership();
+        System.out.println("\n ****** JAVA DEALERSHIP! ****** \n");
+        System.out.print("Welcome! Enter the type of car you're looking for: ");
+        String make1 = scan.nextLine();
+        System.out.print("Enter your budget: ");
+        int budget = scan.nextInt();
         for (int i = 0; i < cars.length; i++) {
             dealership.setCar(cars[i], i);
         }
-
+scan.close();
     }
 }
